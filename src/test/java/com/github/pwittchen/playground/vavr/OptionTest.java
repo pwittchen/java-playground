88c9f5578d1778c6f40bf0aaae8b4140f36c6091
Piotr Wittchen <piotr.wittchen@sap.com>
2018-02-05 16:23:02 +0100

    updating comments

--- src/test/java/com/github/pwittchen/playground/vavr/OptionTest.java
+++ src/test/java/com/github/pwittchen/playground/vavr/OptionTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 /**
  * Exploratory Test Case case to verify work of Option type from Vavr
+ * See this article: https://softwaremill.com/do-we-have-better-option-here/
+ * See the docs: http://www.vavr.io/vavr-docs/#_option
  */
 public class OptionTest {
 
