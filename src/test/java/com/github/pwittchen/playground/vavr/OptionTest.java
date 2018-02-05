85e502c5331d6dbce771cdacbc4fb1d8d36ed3ae
Piotr Wittchen <piotr.wittchen@sap.com>
2018-02-05 16:10:07 +0100

    adding another class for test case

---
+++ src/test/java/com/github/pwittchen/playground/vavr/OptionTest.java
@@ -0,0 +1,14 @@
+package com.github.pwittchen.playground.vavr;
+
+import org.junit.Test;
+
+/**
+ * Exploratory Test Case case to verify work of Option type from Vavr
+ */
+public class OptionTest {
+
+	@Test
+	public void shouldTestOption() {
+		//TODO: implement
+	}
+}
