dde90e622775e4ade10b1ca9dfe44d854d005598
Piotr Wittchen <piotr.wittchen@sap.com>
2018-02-05 13:52:20 +0100

    adding project skeleton in Gradle

---
+++ src/main/java/com/github/pwittchen/playground/Main.java
@@ -0,0 +1,7 @@
+package com.github.pwittchen.playground;
+
+public class Main {
+	public static void main(final String[] args) {
+		System.out.println("application started");
+	}
+}
---
+++ src/test/java/com/github/pwittchen/playground/MainTest.java
@@ -0,0 +1,13 @@
+package com.github.pwittchen.playground;
+
+import org.junit.Test;
+
+
+public class MainTest {
+
+	@Test
+	public void shouldTestSomething() {
+		//TODO: implement
+	}
+
+}
