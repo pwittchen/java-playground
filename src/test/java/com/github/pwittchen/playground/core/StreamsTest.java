56d573350c23c999b99c2988aeb7c08e4e6c5f58
Piotr Wittchen <piotr.wittchen@sap.com>
2018-02-20 17:01:05 +0100

    adding sample test with map to StreamsTest

--- src/test/java/com/github/pwittchen/playground/core/StreamsTest.java
+++ src/test/java/com/github/pwittchen/playground/core/StreamsTest.java
@@ -1,5 +1,34 @@
 package com.github.pwittchen.playground.core;
 
+import com.google.common.collect.ImmutableMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+
 public class StreamsTest {
-  //TODO: implement
+  @Test
+  public void shouldProduceMappedStreamWithDuplicatedValues() {
+
+    // given
+    final Stream<Integer> stream = Stream.of(1, 2, 3, 4, 4, 4, 5);
+    final Map<Integer, String> map =
+        ImmutableMap.of(
+            1, "one",
+            2, "two",
+            3, "three",
+            4, "four",
+            5, "five");
+
+    // when
+    final List<String> list = stream
+        .map(map::get)
+        .collect(Collectors.toList());
+
+    // then
+    assertThat(list.size()).isEqualTo(7);
+  }
 }
