2c729631bb62b26b16e361028062f5c85a26605c
Piotr Wittchen <piotr.wittchen@sap.com>
2018-03-05 17:14:17 +0100

    adding examples with timing proxy

--- src/test/java/com/github/pwittchen/playground/core/DynamicProxyTest.java
+++ src/test/java/com/github/pwittchen/playground/core/DynamicProxyTest.java
@@ -3,6 +3,7 @@ package com.github.pwittchen.playground.core;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.HashMap;
 import java.util.Map;
 import org.junit.Test;
 
@@ -51,6 +52,17 @@ public class DynamicProxyTest {
     // then an exception is thrown
   }
 
+  @Test
+  public void shouldInvokeTimingProxy() {
+    final Map mapProxyInstance = createTimingProxyInstance();
+    final CharSequence csProxyInstance = createCharSequenceProxyInstance();
+
+    mapProxyInstance.put("hello", "world");
+    csProxyInstance.length();
+
+    // watch console logs to see outputs for this test
+  }
+
   private Map createProxyInstance() {
     return (Map) Proxy.newProxyInstance(
         DynamicProxyTest.class.getClassLoader(),
@@ -74,10 +86,49 @@ public class DynamicProxyTest {
   }
 
   class DynamicInvocationHandler implements InvocationHandler {
+
     @Override
     public Object invoke(final Object proxy, final Method method, final Object[] args) {
       System.out.println(String.format("Invoked method: %s", method.getName()));
       return 42;
+    }
+  }
+
+  private Map createTimingProxyInstance() {
+    return (Map) Proxy.newProxyInstance(
+        DynamicProxyTest.class.getClassLoader(), new Class[] { Map.class },
+        new TimingDynamicInvocationHandler(new HashMap<>()));
+  }
+
+  private CharSequence createCharSequenceProxyInstance() {
+    return (CharSequence) Proxy.newProxyInstance(
+        DynamicProxyTest.class.getClassLoader(),
+        new Class[] { CharSequence.class },
+        new TimingDynamicInvocationHandler("Hello World"));
+  }
+
+  class TimingDynamicInvocationHandler implements InvocationHandler {
+
+
+    private final Map<String, Method> methods = new HashMap<>();
+    private Object target;
+
+    public TimingDynamicInvocationHandler(final Object target) {
+      this.target = target;
+
+      for (final Method method : target.getClass().getDeclaredMethods()) {
+        this.methods.put(method.getName(), method);
+      }
+    }
+
+    @Override
+    public Object invoke(final Object proxy, final Method method, final Object[] args)
+        throws Throwable {
+      final long start = System.nanoTime();
+      final Object result = methods.get(method.getName()).invoke(target, args);
+      final long elapsed = System.nanoTime() - start;
+      System.out.println(String.format("Executing %s finished in %s ns", method.getName(), elapsed));
+      return result;
     }
   }
 }
