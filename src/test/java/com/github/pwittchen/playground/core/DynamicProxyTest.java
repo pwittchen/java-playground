8a84bf7edd36c165f8f408586ec27ffdfc78ee08
Piotr Wittchen <piotr.wittchen@sap.com>
2018-03-05 16:55:44 +0100

    making code of tests for DynamicProxy more clear

--- src/test/java/com/github/pwittchen/playground/core/DynamicProxyTest.java
+++ src/test/java/com/github/pwittchen/playground/core/DynamicProxyTest.java
@@ -6,6 +6,8 @@ import java.lang.reflect.Proxy;
 import java.util.Map;
 import org.junit.Test;
 
+import static com.google.common.truth.Truth.assertThat;
+
 /**
  * Read more at: http://www.baeldung.com/java-dynamic-proxies
  */
@@ -14,18 +16,51 @@ public class DynamicProxyTest {
   @Test
   public void shouldInvokePutMethodViaProxy() {
 
-    final Map proxyInstance = (Map) Proxy.newProxyInstance(
+    // given
+    final Map proxyInstance = createProxyInstance();
+
+    // when
+    final Integer returnedValue = (Integer) proxyInstance.put("hello", "world");
+
+    // then
+    assertThat(returnedValue).isEqualTo(42);
+  }
+
+  @Test
+  public void shouldInvokeGetFromProxyViaLambda() {
+
+    // given
+    final Map proxyInstance = createLambdaProxyInstance();
+
+    // when
+    final Integer returnedValue = (Integer) proxyInstance.get("hello");
+
+    // then
+    assertThat(returnedValue).isEqualTo(42);
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void shouldInvokePutFromProxyViaLambdaAndThrowException() {
+
+    // given
+    final Map proxyInstance = createLambdaProxyInstance();
+
+    // when
+    proxyInstance.put("hello", "world");
+
+    // then an exception is thrown
+  }
+
+  private Map createProxyInstance() {
+    return (Map) Proxy.newProxyInstance(
         DynamicProxyTest.class.getClassLoader(),
         new Class[] {Map.class},
         new DynamicInvocationHandler()
     );
-
-    proxyInstance.put("hello", "world");
   }
 
-  @Test
-  public void shouldInvokeProxyViaLambda() {
-    final Map proxyInstance = (Map) Proxy.newProxyInstance(
+  private Map createLambdaProxyInstance() {
+    return (Map) Proxy.newProxyInstance(
         DynamicProxyTest.class.getClassLoader(),
         new Class[] {Map.class},
         (proxy, method, methodArgs) -> {
@@ -33,15 +68,12 @@ public class DynamicProxyTest {
             System.out.println(String.format("Invoked method: %s", method.getName()));
             return 42;
           } else {
-            throw new UnsupportedOperationException(
-                "Unsupported method: " + method.getName());
+            throw new UnsupportedOperationException("Unsupported method: " + method.getName());
           }
         });
-
-    proxyInstance.get("hello");
   }
 
-  public class DynamicInvocationHandler implements InvocationHandler {
+  class DynamicInvocationHandler implements InvocationHandler {
     @Override
     public Object invoke(final Object proxy, final Method method, final Object[] args) {
       System.out.println(String.format("Invoked method: %s", method.getName()));
