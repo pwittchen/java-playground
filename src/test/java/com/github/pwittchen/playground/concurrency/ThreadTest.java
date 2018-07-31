a5df112cd5aa8e7c5eae3b0fe32e2bd875f96228
Piotr Wittchen <piotr.wittchen@sap.com>
2018-07-31 15:51:57 +0200

    adding more notes and test for Thread

--- src/test/java/com/github/pwittchen/playground/concurrency/ThreadTest.java
+++ src/test/java/com/github/pwittchen/playground/concurrency/ThreadTest.java
@@ -23,6 +23,12 @@ public class ThreadTest {
 
   @Test
   public void shouldCreateDaemonThread() throws InterruptedException {
+
+    // note: daemon threads should be used for house-keeping tasks or tasks,
+    // which can be interrupted without consequences - e.g. periodic checks, etc.
+    // daemon tasks have low priority and JVM can kill them anytime
+    // no matter if they're finished or not
+
     DaemonThread thread = new DaemonThread();
     thread.run();
     Thread.sleep(2000);
@@ -42,5 +48,22 @@ public class ThreadTest {
       System.out.println("executed: " + Thread.currentThread().toString());
       System.out.println("is daemon: " + isDaemon());
     }
+  }
+
+  @Test
+  public void shouldCreateThreadLocal() {
+
+    // note: ThreadLocal has value which is 'internal'
+    // for a given thread and won't be shared among different threads
+
+    Thread thread = new Thread(() -> {
+      ThreadLocal<Integer> threadLocal = ThreadLocal.withInitial(() -> 1);
+      threadLocal.set(threadLocal.get() + 1);
+      System.out.println(threadLocal.get());
+    });
+
+    thread.run();
+    thread.run();
+
   }
 }
